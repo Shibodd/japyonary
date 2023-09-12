0d5c1954849b678fd640dcb5e471e110c5443731
Shibodd <simonebondi456@gmail.com>
2023-09-12 18:09:59 +0200

    SRS link in homepage

--- srs/templatetags/srs_filters.py
+++ srs/templatetags/srs_filters.py
@@ -2,9 +2,14 @@ from django import template
 from dictionary.models import Entry
 from srs.models import Flashcard
 from users.models import User
+from django.utils import timezone
 
 register = template.Library()
 
 @register.filter
 def is_in_user_srs(entry: Entry, user: User):
   return Flashcard.objects.filter(owner=user, entry=entry).exists()
+
+@register.filter
+def pending_reviews_count(user: User):
+  return Flashcard.objects.expired(user, timezone.now()).count()
