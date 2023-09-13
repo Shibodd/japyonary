fc9b8c897bc7943af85b3bb9f4442fe8a9ab0238
Shibodd <simonebondi456@gmail.com>
2023-09-13 14:32:52 +0200

    Basic setup

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # japyonary
 
 My project for the university's Web Technologies final exam.
+
+mkdir japyonary
+cd japyonary
+git clone REPO_URL
+python3 -m venv env
+cd japyonary
+source setup.sh
---
+++ setup.sh
@@ -0,0 +1,17 @@
+set -e
+
+ROOT=$(dirname "$0")
+JMDICT_VERSION=JMdict_e
+JMDICT_DOWNLOAD_FOLDER=$ROOT/JMdict
+
+# Install all dependencies
+python3 -m pip install -r requirements.txt
+
+# Download and unzip the JMdict
+wget -c -P $JMDICT_DOWNLOAD_FOLDER http://ftp.edrdg.org/pub/Nihongo/$JMDICT_VERSION.gz
+gzip -fdk $JMDICT_DOWNLOAD_FOLDER/$JMDICT_VERSION.gz
+
+# Make migrations and migrate
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py load_dictionary $JMDICT_DOWNLOAD_FOLDER/$JMDICT_VERSION
