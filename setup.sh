359f32beb46273d60b3bdc792685aa6ad1bca633
Shibodd <simonebondi456@gmail.com>
2023-09-13 14:51:19 +0200

    Setup messages

--- setup.sh
+++ setup.sh
@@ -5,17 +5,22 @@ ROOT=$(dirname "$0")
 JMDICT_VERSION=JMdict_e
 JMDICT_DOWNLOAD_FOLDER=$ROOT/JMdict
 
+# Create the virtual environment
+echo "Creating virtual environment..."
 python3 -m venv $VENV_NAME
 source $VENV_NAME/bin/activate
 
 # Install all dependencies
+echo "Installing dependencies..."
 python3 -m pip install -r requirements.txt
 
 # Download and unzip the JMdict
+echo "Downloading the dictionary..."
 wget -c -P $JMDICT_DOWNLOAD_FOLDER http://ftp.edrdg.org/pub/Nihongo/$JMDICT_VERSION.gz
 gzip -fdk $JMDICT_DOWNLOAD_FOLDER/$JMDICT_VERSION.gz
 
 # Make migrations and migrate
+echo "Setting up the DB..."
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py load_dictionary $JMDICT_DOWNLOAD_FOLDER/$JMDICT_VERSION
